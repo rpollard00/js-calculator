7241072093e187a4dc5a8863c71ee8c22fba41af
Reese Pollard <rpollard@gmail.com>
2022-07-25 16:10:41 -0700

    Additional changes

--- script.js
+++ script.js
@@ -20,11 +20,7 @@ const modulo = function(a, b) {
 }
 
 const power = function(x, n) {
-  let raised = x;
-	for (i=1; i < n; i++) {
-    raised *= x;
-  }
-  return raised;
+  return x ** n;
 };
 
 const factorial = function(val) {
@@ -46,4 +42,48 @@ const calc = {
     "%": modulo,
     "^": power,
     "!": factorial
+};
+
+// uses calc object and operator character to do the calculation
+const operate = function(operator, a, b) {
+    return calc[operator](a, b);
+};
+
+function addClass(key, className) {
+    key.classList.add(className);
+};
+
+function removeClass(key, className) {
+    key.classList.remove(className);
+};
+
+function runCalc(inputVal) {
+    // if charater is a number then append display
+    console.log('Text content: ' + display.textContent);
+    console.log('Number(text): ' + Number(inputVal));
+    if (Number(inputVal)) {
+        
+        if ((display.textContent) === "" && (Number(inputVal) === 0)) {
+            console.log("Do nothing");
+        }
+        else {
+            display.textContent += inputVal.toString();
+        }
+    } else {
+        display.textContent = inputVal.toString();
+    }
+    // if character is an operator then refresh display
 }
+
+const display = document.querySelector("#display");
+display.textContent = "";
+const container = document.querySelector('#container');
+const keypad = document.querySelectorAll('.keypad');
+console.log(keypad);
+
+keypad.forEach(key => key.addEventListener('click', () => {
+    runCalc(key.textContent);
+}));
+
+keypad.forEach(key => key.addEventListener('mouseover', () => addClass(key,'hover')));
+keypad.forEach(key => key.addEventListener('mouseout', () => removeClass(key,'hover')));
